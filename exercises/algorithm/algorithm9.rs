333639a28a6b20ffb248841bab0d790abfec189c
no_to_last <futurehadt@outlook.com>
2025-03-26 20:11:10 +0800

    110

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,7 +36,19 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        // 添加元素到数组末尾
+        self.items.push(value);
+        self.count += 1;
+        
+        // 向上调整堆（sift up）
+        let mut idx = self.count;  // 新元素的索引
+        
+        // 当前元素不是根节点且满足比较条件时进行交换
+        while idx > 1 && (self.comparator)(&self.items[idx], &self.items[self.parent_idx(idx)]) {
+            let parent_idx = self.parent_idx(idx);
+            self.items.swap(idx, parent_idx);
+            idx = parent_idx;
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +68,28 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        // 获取左子节点索引
+        let left_idx = self.left_child_idx(idx);
+        
+        // 如果左子节点不存在，返回0（表示没有子节点）
+        if left_idx > self.count {
+            return 0;
+        }
+        
+        // 获取右子节点索引
+        let right_idx = self.right_child_idx(idx);
+        
+        // 如果右子节点不存在，返回左子节点索引
+        if right_idx > self.count {
+            return left_idx;
+        }
+        
+        // 比较左右子节点，返回满足比较条件的索引
+        if (self.comparator)(&self.items[left_idx], &self.items[right_idx]) {
+            left_idx
+        } else {
+            right_idx
+        }
     }
 }
 
@@ -84,8 +115,42 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        // 如果堆为空，返回None
+        if self.is_empty() {
+            return None;
+        }
+        
+        // 保存根节点值（堆顶元素）
+        let root = std::mem::take(&mut self.items[1]);
+        
+        // 如果堆中只有一个元素，直接移除它
+        if self.count == 1 {
+            self.count = 0;
+            return Some(root);
+        }
+        
+        // 将最后一个元素移到根节点位置
+        self.items[1] = self.items.pop().unwrap();
+        self.count -= 1;
+        
+        // 向下调整堆（sift down）
+        let mut idx = 1;
+        
+        // 当存在子节点时进行调整
+        while self.children_present(idx) {
+            let child_idx = self.smallest_child_idx(idx);
+            
+            // 如果子节点满足比较条件，交换节点
+            if child_idx != 0 && (self.comparator)(&self.items[child_idx], &self.items[idx]) {
+                self.items.swap(idx, child_idx);
+                idx = child_idx;
+            } else {
+                // 否则，堆已经平衡，退出循环
+                break;
+            }
+        }
+        
+        Some(root)
     }
 }
 
